e837c12ce4f0ab283e1c036b7e67394319603729
realmayus <realmayus@gmail.com>
2021-01-18 14:14:13 +0100

    Added CORS whitelist

--- main.py
+++ main.py
@@ -10,7 +10,31 @@ app = Flask(__name__)
 config = configparser.ConfigParser()
 config.read("./config.ini")
 libreoffice_binary = config["config"]["libreoffice_bin"]
+allow = config["config"]["allow_origins"].split(",")
 
+
+#  const allowedOrigins = ['http://localhost:8081', 'http://localhost:8080'];
+#  const origin = req.headers.origin;
+#  if (allowedOrigins.includes(origin)) {
+#    res.setHeader('Access-Control-Allow-Origin', origin);
+#  }
+#  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+#  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+#  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+#  res.header('Access-Control-Allow-Credentials', "true");
+
+@app.after_request
+def add_cors_headers(response):
+    r = request.referrer[:-1]
+    if r in allow:
+        response.headers.add('Access-Control-Allow-Origin', r)
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+        response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
+        response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    return response
 
 @app.route("/", methods=["POST"])
 def hello():
